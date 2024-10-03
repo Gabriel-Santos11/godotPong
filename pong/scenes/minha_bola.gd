8f71c9be14b18c1d6a59709c580ddce97145e691
Gabriel-Santos11 <82913389+Gabriel-Santos11@users.noreply.github.com>
2024-10-02 21:53:36 -0300

    first commit :tada:

---
+++ pong/scenes/minha_bola.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+var speed: Vector2 = Vector2(400, 0)  # Velocidade inicial da bolinha
+
+# Chamado quando o nó entra na árvore de cena pela primeira vez.
+func _ready() -> void:
+	speed = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * 400
+	set_linear_velocity(speed)
+
+
+# Chamado quando a bolinha colide com outro corpo.
+func _on_Ball_body_entered(body):
+	if body.is_in_group("paddle"):
+		# Refletir a direção
+		var normal = (position - body.position).normalized()
+		var new_velocity = speed.bounce(normal)
+		set_linear_velocity(new_velocity)
